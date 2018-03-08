c9c8ebbefc2135108800a052cdea79dd7f7f4d71
Ferdous Azad <ferdousazad12@gmail.com>
2018-03-08 13:16:57 +0600

    Slides setup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController,Slides } from 'ionic-angular';
+import { MainPage }  from '../main/main';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(Slides) slides: Slides;
+  skipMsg: string = "Skip";
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  skip() {
+    this.navCtrl.push(MainPage);
+  }
+
+  slideChanged(){
+    if(this.slides.isEnd()){
+      this.skipMsg="You're all set!";
+    }
+  }
 }
