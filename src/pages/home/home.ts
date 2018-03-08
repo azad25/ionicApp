fd0a8ff34d5883dd1138a72cb59f31429a401416
Ferdous Azad <ferdousazad12@gmail.com>
2018-03-08 13:51:36 +0600

    Animations added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, trigger, transition, style, state,animate, keyframes } from '@angular/core';
 import { NavController,Slides } from 'ionic-angular';
 import { MainPage }  from '../main/main';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  animations: [
+    trigger('bounce', [
+      state('*', style({
+        transform: 'translateX(0)'
+      })),
+      transition('* => rightSwipe', animate('.7s ease-out', keyframes([
+        style({ transform: 'translateX(0)', offset:0 }),
+        style({ transform: 'translateX(-65px)', offset:0.3 }),
+        style({ transform: 'translateX(0)', offset:1 })
+      ]))),
+      transition('* => leftSwipe', animate('0.7s ease-out', keyframes([
+        style({ transform: 'translateX(0)', offset:0 }),
+        style({ transform: 'translateX(65px)', offset:0.3 }),
+        style({ transform: 'translateX(0)', offset:1 })
+      ])))
+    ])
+  ]
 })
 export class HomePage {
   @ViewChild(Slides) slides: Slides;
   skipMsg: string = "Skip";
+  state: string='x';
 
   constructor(public navCtrl: NavController) {
 
@@ -22,5 +40,17 @@ export class HomePage {
     if(this.slides.isEnd()){
       this.skipMsg="You're all set!";
     }
+  }
+
+  slideMoved(){
+    if(this.slides.getActiveIndex() >= this.slides.getPreviousIndex()){
+      this.state = "rightSwipe";
+    }else{
+      this.state = "leftSwipe";      
+    }
+  }
+
+  animationsDone(){
+    this.state = "x";
   }
 }
